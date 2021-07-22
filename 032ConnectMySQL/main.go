0ade1cfe11a088a5ec5214eedb103268dcfe0f9f
hewei <hemailads@163.com>
2021-07-22 09:41:00 +0800

    update

--- 032ConnectMySQL/main.go
+++ 032ConnectMySQL/main.go
@@ -51,12 +51,12 @@ func main() {
 	}
 
 	//插入
-	sql := "INSERT INTO table_json (content) VALUES (?)"
+	s := "INSERT INTO table_json (content) VALUES (?)"
 	str, err := json.Marshal([10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0})
 	if err != nil {
 		panic(err)
 	}
-	result, err := db.Exec(sql, str)
+	result, err := db.Exec(s, str)
 	if err != nil {
 		panic(err)
 	}
